87f836d389b44d801a7de9a525e1faca1482d39c
djshujja <djshujja@gmail.com>
2020-10-01 17:22:52 +0500

    added fetch GET and POST

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 function onSlideMenu() {
   document.getElementById("menu").style.width = "250px";
   document.getElementById("content").style.marginLeft = "250px";
@@ -5,4 +7,83 @@ function onSlideMenu() {
 function closeSlideMenu() {
   document.getElementById("menu").style.width = "0";
   document.getElementById("content").style.marginLeft = "0";
+}
+
+var name,prodID,qty,unitPrice,unit,purchasedPrice;
+
+async function submitData(){
+  name = document.getElementById("productName").value;
+  prodID = document.getElementById("productId").value;
+  qty = document.getElementById("qty").value;
+  unit = document.getElementById("unit").value;
+  unitPrice = document.getElementById("unitPrice").value;
+  purchasedPrice = document.getElementById("purchasedPrice").value;
+
+  data = {name: name, productId:prodID, qty:qty, unit:unit, unitPrice:unitPrice, purchasedPrice:purchasedPrice}
+  console.log(data)
+ await fetch('http://localhost:8000/inventory', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  await renderProducts();
+
+}
+
+
+
+
+async function API(){
+
+  // fetch('http://localhost:8000/inventory').then(response => response.json())  // convert to json
+  //   .then(json => console.log(json))    //print data to console
+  //   .catch(err => console.log('Request Failed', err)); // Catch errors
+  
+  console.log('API Called')
+  await fetch('http://localhost:8000/inventory')
+  .then(response => response.json())
+  .then(json => {
+    return data = json
+    // for(i=0; i<json.length; i++){
+      
+    //   // console.log(json[i])
+    //   // return json[i]
+    // }
+    // console.log(data)
+    // return data
+  })
+  // console.log(data)
+  return data;
+}
+
+
+async function renderProducts(){
+  let a = await API();
+  
+  let html = ''
+  a.forEach(product => {
+    let htmlSegment = `
+            <tr>
+              <th scope="row">${product.productId}</th>
+              <td>${product.name}</td>
+              <td>${product.qty}</td>
+              <td>${product.unit}</td>
+            </tr>
+            `
+
+  html += htmlSegment;
+  })
+  
+  let productsRow = document.querySelector('.products-row');
+  productsRow.innerHTML = html
+
 }
